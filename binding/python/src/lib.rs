4cb89bedad60c8c88fb42f56a337e56478624b9e
Luca Vivona <lucavivona01@gmail.com>
2025-03-26 03:08:53 -0400

    style: clippy fix

--- binding/python/src/lib.rs
+++ binding/python/src/lib.rs
@@ -129,18 +129,18 @@ fn serialize<'py>(
     Ok(pybytes)
 }
 
-/// Serializes raw data.
-///
-/// Args:
-///     tensor_dict (`Dict[str, Dict[Any]]`):
-///         The tensor dict is like:
-///             {"tensor_name": {"dtype": "F32", "shape": [2, 3], "data": b"\0\0"}}
-///     metadata (`Dict[str, str]`, *optional*):
-///         The optional purely text annotations
-///
-/// Returns:
-///     (`tulpel(bytes, bytes)`):
-///         The serialized content, with a sha1 checksum hash .
+// /// Serializes raw data.
+// ///
+// /// Args:
+// ///     tensor_dict (`Dict[str, Dict[Any]]`):
+// ///         The tensor dict is like:
+// ///             {"tensor_name": {"dtype": "F32", "shape": [2, 3], "data": b"\0\0"}}
+// ///     metadata (`Dict[str, str]`, *optional*):
+// ///         The optional purely text annotations
+// ///
+// /// Returns:
+// ///     (`tulpel(bytes, bytes)`):
+// ///         The serialized content, with a sha1 checksum hash .
 // #[pyfunction]
 // #[pyo3(signature = (tensor_dict, metadata=None))]
 // fn serialize_checksum<'py>(
