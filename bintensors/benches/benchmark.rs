bd9f0716f1aef5de638048399c43fc5ad158f286
Luca Vivona <lucavivona01@gmail.com>
2025-03-30 23:39:32 -0400

    style: cargo fmt

--- bintensors/benches/benchmark.rs
+++ bintensors/benches/benchmark.rs
@@ -1,5 +1,5 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use bintensors::tensor::*;
+use criterion::{Criterion, black_box, criterion_group, criterion_main};
 use std::collections::HashMap;
 
 // Returns a sample data of size 2_MB
